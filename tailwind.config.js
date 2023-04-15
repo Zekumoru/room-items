6b2a26cc4f493f847563e680b9bcedc7e26ee412
Zekumoru Dragonhart <dev@zekumoru.com>
2023-04-15 19:33:49 +0200

    Extend colors with primary and secondary

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          600: '#4A6572',
+          700: '#344955',
+          800: '#232F34',
+        },
+        secondary: {
+          500: '#F9AA33',
+        },
+      },
+    },
   },
   plugins: [],
 };
